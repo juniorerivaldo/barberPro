f8b9ecc861dc1e2fee33270a084aa886a9ad0ed5
Erivaldo Junior <esttstart@gmail.com>
2023-05-20 11:28:51 -0300

    adicionando retorno de detalhes do user logado

--- src/controllers/user/DetailUserController.ts
+++ src/controllers/user/DetailUserController.ts
@@ -3,8 +3,9 @@ import { UserDetailService } from "../../services/user/DetailUserService";
 
 class DetailUserController {
   async handle(request: Request, response: Response) {
+    const user_id = request.user_id
     const userDetailService = new UserDetailService();
-    const detailUser = await userDetailService.execute();
+    const detailUser = await userDetailService.execute(user_id);
 
     return response.json(detailUser);
   }
