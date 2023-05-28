3ddcb11a08b3667c51c18bf76d30c1d038ea9eb7
Erivaldo Junior <esttstart@gmail.com>
2023-05-28 16:40:48 -0300

    iniciando front-end e separando do back-end

---
+++ front-end/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>BarberPro- seu sistema completo</title>
+      </Head>
+      <div>
+        <h1>TESTE PROJETO</h1>
+      </div>
+    </>
+  );
+}
