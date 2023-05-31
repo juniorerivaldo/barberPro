4e67d60be76d6dc90f5cede74c702651d13c5ad0
Erivaldo Junior <esttstart@gmail.com>
2023-05-31 14:26:41 -0300

    batendo token com backend

--- front-end/src/context/AuthContext.tsx
+++ front-end/src/context/AuthContext.tsx
@@ -1,5 +1,5 @@
-import { createContext, ReactNode, useState } from "react";
-import { destroyCookie, setCookie } from "nookies";
+import { createContext, ReactNode, useState, useEffect } from "react";
+import { destroyCookie, setCookie, parseCookies } from "nookies";
 import Router from "next/router";
 import { api } from "../services/apiClient";
 
@@ -53,6 +53,18 @@ export function signOut() {
 export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<UserProps>();
   const isAuthenticated = !!user; // converte a variavel acima para bool
+
+  useEffect(() => {
+    const { "@barber.token": token } = parseCookies();
+    if (token) {
+      api.get("/me").then((response) => {
+        const { id, name, endereco, email, subscriptions } = response.data;
+        setUser({ id, name, endereco, email, subscriptions });
+      }).catch(() => {
+        signOut()
+      })
+    }
+  }, []);
 
   async function signIn({ email, password }: SignInProps) {
     try {
